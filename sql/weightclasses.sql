3428a7e5c8e82d72d2015c5dd1a390adbd25ac66
Jean Richard <jean@geemoo.ca>
2011-08-14 13:00:31 -0400

    individual sql files as they were created

---
+++ sql/weightclasses.sql
@@ -0,0 +1,7 @@
+insert into weightclass (id, name, lbs) values (1, 'Bantamweight', 135);
+insert into weightclass (id, name, lbs) values (2, 'Featherweight', 145);
+insert into weightclass (id, name, lbs) values (3, 'Lightweight', 155);
+insert into weightclass (id, name, lbs) values (4, 'Welterweight', 170);
+insert into weightclass (id, name, lbs) values (5, 'Middleweight', 185);
+insert into weightclass (id, name, lbs) values (6, 'Light Heavyweight', 205);
+insert into weightclass (id, name, lbs) values (7, 'Heavyweight', 265);
